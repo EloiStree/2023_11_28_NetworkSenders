7a95bce0f10dbf1afba3aa3f589d04dbaecae51a
Eloi Stree <eloistree@gmail.com>
2024-06-28 02:02:25 +0200

    Save: 06/28/2024 2:02 AM

--- Runtime/UdpSenderRawByteArray.cs
+++ Runtime/UdpSenderRawByteArray.cs
@@ -12,7 +12,7 @@ public class UdpSenderRawByteArray : MonoBehaviour
     public int m_port = 12345;
 
     private UdpClient m_udpClient;
-
+    public ulong m_sent;
     IPEndPoint groupEP;
     void Awake()
     {
@@ -31,13 +31,24 @@ public class UdpSenderRawByteArray : MonoBehaviour
             m_messageToSendCount= m_messageToSend.Count;
         }
     }
+    public void SendInteger(int integer)
+    {
+        SendRawByteArray(BitConverter.GetBytes(integer));
+    }
+    [ContextMenu("Send Random Integer")]
+    public void SendRandomInteger()
+    {
+        SendRawByteArray(BitConverter.GetBytes(UnityEngine.Random.Range(int.MinValue, int.MaxValue)));
+    }
     public void Update()
     {
         while(m_messageToSend.Count > 0)
         {
             byte[] data = m_messageToSend.Dequeue();
             m_udpClient.Send(data, data.Length, groupEP);
+            m_sent++;
         }
+        m_messageToSendCount = 0;
     }
 
 
